ba92f4f1b519d546fc56ac8c2b55e1751d7d9bcd
rigauxh <heloise.rigaux@etu.umontpellier.fr>
2024-12-05 21:31:46 +0100

    feat : structure de base fait

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,7 +1,37 @@
 let nameListening = document.getElementById("input_id");
+nameListening.addEventListener("keypress", func);
 let name = document.getElementById("input_id").value;
-nameListening.oninput = () => {
-    name = document.getElementById("input_id").value;
-    console.log('The value is : ', name);
-    document.getElementById("input_id").innerText.replace("f", "test")
+
+function func() {
+    if (generateRandomChance() < 5){
+        name = document.getElementById("input_id").value;
+        document.getElementById("input_id").value = replaceChar(generateRandomIndex(),generateRandomChar());
+    }
+}
+
+function replaceChar(index, char) {
+    if (name.length === 0){
+        return "";
+    }
+    return name.substring(0, index - 1) + char + name.substring(index, name.length);
+}
+
+function generateRandomIndex(){
+    if (name.length === 0){
+        return 0;
+    }
+    return Math.floor(Math.random() * name.length + 1);
+}
+
+function generateRandomChance(){
+    return Math.floor(Math.random() * 10+1);
+}
+
+function generateRandomChar (){
+    let result = '';
+    const characters = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN1234567890&é-è_çà=*!:;,?./§';
+    // Loop to generate characters for the specified length
+    const randomInd = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomInd);
+    return result;
 }
