c0f9b1edd7236f007041d0a415d1015193fa2e9b
Boll <bollsudomaster@gmail.com>
2024-12-06 03:24:48 +0100

    feat: implémentation du style de la page de base (ripple effect, bulles, blur...)

---
+++ assets/js/background.js
@@ -0,0 +1,29 @@
+const aboveTheOcean = document.getElementById("aboveTheOcean");
+let blurValue = 0;
+
+
+let timer;
+let is8bit = false;
+startTimer();
+
+function startTimer(interval = 1000) {
+    timer = setInterval(function () {
+        blurValue = "blur("+Math.abs(gaussianRandom(0, 3))+"px)";
+        aboveTheOcean.style.filter = blurValue;
+    }, interval);
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function gaussianRandom(mean=0, variance=1) {
+    const u = 1 - Math.random();
+    const v = Math.random();
+    const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    return z * variance + mean;
+}
+
+
+
+
